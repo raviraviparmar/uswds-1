75ecc1727a1436495b8c6a3d89c2c7e7c30fe9d1
raviraviparmar <110527090+raviraviparmar@users.noreply.github.com>
2026-02-05 17:50:12 +0530

    Update utility-popular-services.php
    utility popular services file changes

--- src-php/utility-popular-services.php
+++ src-php/utility-popular-services.php
@@ -9,22 +9,19 @@
             <div class="desktop:grid-col-6 tablet:grid-col-6">
                 <a
                     href="#"
-                    class="usa-button flex-justify-start height-7 width-full bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black"
-                    style="background-image: url('uswds/dist/img/usa-icons/navigate_next.svg'); background-repeat: no-repeat; background-position: right 1.25rem center;">
+                    class="usa-button quick-link-arrow flex-justify-start height-7 width-full bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black">
                     <span class="font-body-md text-black">Pay A Ticket</span>
                 </a>
 
                 <a
                     href="#"
-                    class="usa-button flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black"
-                    style="background-image: url('uswds/dist/img/usa-icons/navigate_next.svg'); background-repeat: no-repeat; background-position: right 1.25rem center;">
+                    class="usa-button quick-link-arrow flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black">
                     <span class="font-body-md text-black">Pay Utilities</span>
                 </a>
 
                 <a
                     href="#"
-                    class="usa-button flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black"
-                    style="background-image: url('uswds/dist/img/usa-icons/navigate_next.svg'); background-repeat: no-repeat; background-position: right 1.25rem center;">
+                    class="usa-button quick-link-arrow flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black">
                     <span class="font-body-md text-black">Public Transit</span>
                 </a>
             </div>
@@ -32,22 +29,19 @@
             <div class="desktop:grid-col-6 tablet:grid-col-6">
                 <a
                     href="#"
-                    class="usa-button flex-justify-start height-7 width-full margin-top-1 tablet:margin-0 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black"
-                    style="background-image: url('uswds/dist/img/usa-icons/navigate_next.svg'); background-repeat: no-repeat; background-position: right 1.25rem center;">
+                    class="usa-button quick-link-arrow flex-justify-start height-7 width-full margin-top-1 tablet:margin-0 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black">
                     <span class="font-body-md text-black">Get A Permit</span>
                 </a>
 
                 <a
                     href="#"
-                    class="usa-button flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black"
-                    style="background-image: url('uswds/dist/img/usa-icons/navigate_next.svg'); background-repeat: no-repeat; background-position: right 1.25rem center;">
+                    class="usa-button quick-link-arrow flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black">
                     <span class="font-body-md text-black">Fire & Rescue</span>
                 </a>
 
                 <a
                     href="#"
-                    class="usa-button flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black"
-                    style="background-image: url('uswds/dist/img/usa-icons/navigate_next.svg'); background-repeat: no-repeat; background-position: right 1.25rem center;">
+                    class="usa-button quick-link-arrow flex-justify-start height-7 width-full margin-top-1 bg-transparent border-bottom-1px border-dotted border-ink hover:bg-transparent hover:border-1px hover:border-black">
                     <span class="font-body-md text-black">Business License</span>
                 </a>
             </div>
